39dc7a29aa79cdd5918056538eaf31d5a5ce6fae
Asepimam <asepimam329@gmail.com>
2026-06-22 07:58:04 +0700

    fix(terminal): restore alt-buffer text selection after scroll/EOF states

--- lib/src/features/ssh_sessions/widget/remote/terminal_workspace_view.dart
+++ lib/src/features/ssh_sessions/widget/remote/terminal_workspace_view.dart
@@ -1,8 +1,10 @@
+import 'dart:developer' as dev;
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:portix/src/connection_manager/session_models.dart'
     as session_models;
@@ -14,9 +16,9 @@ import 'package:xterm/xterm.dart';
 import '../../controller/index.dart';
 import 'terminal_shortcuts.dart';
 
-part 'sections/empty_section.dart';
-part 'sections/workspace_tree_section.dart';
 part 'sections/terminal_pane_section.dart';
 part 'sections/overlay_section.dart';
+part 'sections/empty_section.dart';
 part 'sections/tab_option_section.dart';
 part 'sections/theme_section.dart';
+part 'sections/workspace_tree_section.dart';
